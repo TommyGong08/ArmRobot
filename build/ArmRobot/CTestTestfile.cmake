c9fa880afe9e7c673574f29cefcef7bc5cb505db
TommyGONG08 <103423198@qq.com>
2020-11-23 05:21:12 -0800

    update .gitignore

--- build/ArmRobot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ghl/ArmRobot/src/ArmRobot
-# Build directory: /home/ghl/ArmRobot/build/ArmRobot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ghl/ArmRobot/src
-# Build directory: /home/ghl/ArmRobot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(ArmRobot)
